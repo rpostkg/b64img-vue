e6bf7cf20858b88fd3558158485920b7b18aefc5
rpostkg <190353914+rpostkg@users.noreply.github.com>
2025-12-05 02:48:04 +0200

    Styling

--- src/App.vue
+++ src/App.vue
@@ -56,10 +56,57 @@ const hasContentToDecode = computed(() => {
       <textarea v-model="decodeInput" rows="20" style="width: 100%"></textarea>
       <div v-if="hasContentToDecode">
         <h1>Decoded:</h1>
-        <img :src="decodeInput" alt="Invalid Data" style="max-width: 100%" />
+        <img :src="decodeInput" alt="Invalid Data" style="width: 100%" />
       </div>
     </section>
   </main>
 </template>
 
-<style scoped></style>
+<style>
+body {
+  background-color: #222;
+  color: #ccc;
+  font-family: sans-serif;
+  padding: 20px;
+}
+main {
+  max-width: 50%;
+  margin: 0 auto;
+}
+
+section {
+  background-color: #333;
+  border: 2px solid;
+  border-color: #555 #111 #111 #555;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+button {
+  background: #444;
+  color: #fff;
+  border: 2px solid;
+  border-color: #666 #111 #111 #666;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin-top: 10px;
+  width: 100%;
+}
+
+input {
+  background: #444;
+  color: #fff;
+  border: 2px solid;
+  border-color: #666 #111 #111 #666;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+textarea {
+  color: #fff;
+  background-color: #222;
+  resize: vertical;
+}
+</style>
